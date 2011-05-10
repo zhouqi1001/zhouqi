7ce782d07024a5b787cb88a27e5f4fd85a504b20
Zhou Qi <zhouqi0307@163.com>
2011-05-10 19:47:40 +0800

    c.c

---
+++ c.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    char str[20]="This is a C world!";
+    char *p =str;
+    printf("%s\n",str);
+    getchar();
+
+
+    while (*p !='\0') p++; 
+    
+    for(p--; p !=(str-1);p--)
+    {
+       putchar(*p);         
+    }
+    putchar('\n');
+    return 0;   
+}
--- hello.c
+++ hello.c
@@ -1,10 +1,3 @@
-#include <stdio.h>
-void say_hi(void);
-int main(int argc, const char *argv[])
-{
-    say_hi();
-    return 0;
-}
 
 void say_hi(void);
 {
