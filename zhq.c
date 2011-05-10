bc48588132a39094ef0b03178712287d04563b32
Zhou Qi <zhouqi0307@163.com>
2011-05-10 19:59:46 +0800

    zhq.c

---
+++ zhq.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+ int main(void)
+ {
+     int d = 0;
+     int i = 0;
+     unsigned int mask = (0x01<<(sizeof(int)*8-1));
+
+     puts("Please input a number:");
+     scanf("%d",&d);
+
+     puts("result:");
+    
+     for (i = 0; i <(sizeof(int)*8); i++)        
+     {      
+         printf("%d",((d & mask ) !=0 ? 1 :0));
+         mask >>=1;
+     }
+     printf("\n");
+
+     return 0;
+ }
