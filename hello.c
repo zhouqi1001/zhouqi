cff0c5fb08ac092e9e7ef86fc436c23fa5ae39a0
Zhou Qi <zhouqi0307@163.com>
2011-05-11 11:56:20 +0800

    zhouqi

--- hello.c
+++ hello.c
@@ -1,5 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{   
+    say_hi();
+    return 0;
+}
 
 void say_hi(void);
-{
-    printf("hi everybody\n");
+{                                       
+    printf("hi everybody\n");   
 }
