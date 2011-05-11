99e515d30cc59a262c34d57cb700d4a9940812ef
Zhou Qi <zhouqi0307@163.com>
2011-05-11 20:42:29 +0800

    zifuchuan

---
+++ zifuchuan.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+void str_test(void);
+int main(int argc, const char *argv[])
+{
+    str_test();
+    return 0;
+}
+void str_test(void)
+{
+    char str[100] = "This is a C world!";
+    char *p = str;
+    p = strtok(p," ");
+    while (p !=NULL) 
+    {
+        printf("%s\n",p); 
+        p = strtok(NULL, " ");
+    }
+}
